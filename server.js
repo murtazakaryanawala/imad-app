6e1287976f4ae13a524fe60c93db5e7817f5fc2d
Murtaza Karyanawala <murtaza.k1994@gmail.com>
2017-08-18 15:01:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,6 +123,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName', function(req,res){
+    // articleName == article-one
+    // articles[articleName] == {} Content object for article one
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
