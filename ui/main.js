0f38c3b81195d4dfe4196c85308125673514b3f6
Murtaza Karyanawala <murtaza.k1994@gmail.com>
2017-08-24 10:37:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 
-var button = 
+var button = document.getElementById('btn');
 var counter = 0;
 
+button.onclick = function(){
+    
+    counter=+1;
+    var span = document.getElementById('count');
+    span.innerHtml = counter.toString();
+}
